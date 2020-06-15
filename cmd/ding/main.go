dfdeeb9628377c8adc0fcdd85b55212ffc281a40
zackradisic <zack@theradisic.com>
2020-06-15 02:12:11 -0400

    plays sound after command execution

--- cmd/ding/main.go
+++ cmd/ding/main.go
@@ -5,6 +5,11 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func main() {
@@ -27,4 +32,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
+	f, err := os.Open("sounds/ding.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer streamer.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
 }
